9803b74cb3219736a04237cf84813a6ef9cb77af
BasmaAhmed77 <bm2597692@gmail.com>
2025-11-25 23:48:54 +0200

    updated Problem_2.cpp with its code

---
+++ Task_2/Problem_10.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+
+        int cnt = 0;
+        stack <char> stack;
+        for (auto c : s)
+        {
+            if (c == '(') stack.push(c);
+            else if (!stack.empty())
+            {
+                if (c != stack.top())
+                {
+                    stack.pop();
+                    cnt += 2;
+                }
+            }
+        }
+        return cnt;
+
+    }
+};
